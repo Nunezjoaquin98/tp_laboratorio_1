f8cbcfe3999c914861f4a73e4016d84ec565ad15
Nunezjoaquin98 <40808656+Nunezjoaquin98@users.noreply.github.com>
2019-04-05 17:33:41 -0300

    Add files via upload

--- Calculadora/Funciones.h
+++ Calculadora/Funciones.h
@@ -55,36 +55,3 @@ int funcion_factorial(int opc1) ;
  * \param input Array donde se cargará el texto ingresado
  * \return void
  */
-void funcion_getString(char mensaje[],char input[]);
-
-/**
- * \brief Solicita un texto numérico al usuario y lo devuelve
- * \param mensaje Es el mensaje a ser mostrado
- * \param input Array donde se cargará el texto ingresado
- * \return 1 si el texto contiene solo números
- */
-int funcion_getStringInt(char mensaje[],char input[]);
-
-/**
- * \brief Solicita un texto numérico al usuario y lo devuelve (acepta flotantes)
- * \param mensaje Es el mensaje a ser mostrado
- * \param input Array donde se cargará el texto ingresado
- * \return 1 si el texto contiene solo números
- */
-int funcion_getStringFloat(char mensaje[],char input[]);
-
-/**
- * \brief Verifica si el valor recibido es numérico
- * \param str Array con la cadena a ser analizada
- * \return 1 si es númerico y 0 si no lo es
- *
- */
-int funcion_ValidarNumero(char str[]);
-
-/**
- * \brief Verifica si el valor recibido es numérico aceptando flotantes
- * \param str Array con la cadena a ser analizada
- * \return 1 si es númerico y 0 si no lo es
- *
- */
-int funcion_validarNumeroFlotante(char str[]);
--- Calculadora/main.c
+++ Calculadora/main.c
@@ -1,117 +1,132 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include "Funciones.h"
 
 int main()
 {
-    int opcion;
-    int flag1 = 0 ;
-    int flag2 = 0 ;
-    int flag3= 0;
-    float num1,num2,suma,resta,multi,div,factorial,factorial2 ;
-    char auxNum1[7],auxNum2[7];
-
+    // se declaran las variables a utilizar en el programa
+    int salir = 0;
+    int flag1 = 0;
+    int flag2 = 0;
+    int flag3 = 0;
+    int factorialA,factorialB;
+    float A,B,suma,resta,div,multi;
 
     do
     {
-
-        // Se utilza un switch para realizar cada opcion de la calculadora en cada case.
-        switch(funcion_menu())  // se utiliza funcion menu para que muestre las opciones.
+        switch(funcion_menu())
         {
+        case 1:
 
-        case 1: // se pide el ingreso del operando A.
-            while(!(funcion_getStringFloat("Ingrese el primer operador:\nA: ",auxNum1)))
-            {
-                printf("**Eror** / Debe ingresar un numero.");
-                system("pause");
-
-            }
-            num1 = atof(auxNum1);
-            if(num1)
+            printf("\n\nInserte el Operador A = ");
+            fflush(stdin);
+            scanf("%f",&A);
+            system("pause");
+            system("cls");
+            if(A)
             {
                 flag1++;
             }
+
             break;
-        case 2: // se pide el ingreso de B
-            while(!(funcion_getStringFloat("Ingrese el primer operador:\nB: ",auxNum2)))
-            {
-                printf("**Eror** / Debe ingresar un numero.");
-                system("pause");
-            }
-            num2 = atof(auxNum2);
-            if(num2)
+        case 2:
+            fflush(stdin);
+            printf("\n\nInserte el Operador B = ");
+            scanf("%f",&B);
+            if(B)
             {
                 flag2++;
             }
+            system("pause");
+            system("cls");
             break;
-        case 3: // Calculo los resultados y se valida que haya puesto un valor en a y en b
-            if(flag1 == 0 )
+        case 3:
+            if(flag1 == 0)
             {
-                printf("ERROR / No se ingreso ningun numero en el valor A.") ;
+                printf("\n\n*Error* No se ingreso el operador A.\n\n");
+                system("pause");
+                system("cls");
             }
             else if(flag2 == 0)
             {
-                printf("ERROR / No se ingreso ningun numero en el valor B.") ;
-            }
-            else
-            {
-                suma=funcion_suma(num1,num2);
-                resta=funcion_resta(num1,num2);
-                multi=funcion_multiplicar(num1,num2);
-                div=funcion_dividir(num1,num2);
-                /*factorial=funcion_factorial(num1);
-                factorial2=funcion_factorial(num2); */
-                printf(">>>>>>>>>>>>>> CALCULANDO RESULTADOS <<<<<<<<<<<<<<");
-            }
-            flag3++;
-            break;
-        case 4: // se muestran los resultados y se limpia la pantalla.
-
-
-            if(flag3== 0)
-            {
-                printf("Error / No calculo los resultados.");
+                printf("\n\n*Error* No se ingreso el operador B.\n\n");
                 system("pause");
-                system("cls") ;
-                break;
+                system("cls");
             }
             else
             {
+                suma = funcion_suma(A,B);
+                resta = funcion_resta(A,B);
+                multi = funcion_multiplicar(A,B);
+                if(A >= 0) // se realiza un if para ver si el numero es mayor o igual a 0 para poder realizar el factorial
+                {
+                factorialA = funcion_factorial(A);
+                }
+                if(B >= 0)
+                {
+                factorialB = funcion_factorial(B);
+                }
+
+                if(B!=0) // se verifica que el divisor no sea 0
+                {
+                    div = funcion_dividir(A,B);
+                }
+                printf("\nCalculando resultados.:.:.:.:.:.:.:.:.:..:.:.:.:.:.:.:.:.:..:.:.:.:.:.:.:.:.:.\n\n");
+                flag3++;
+                system("pause");
+                system("cls");
+
+            }
+            break;
+        case 4:
+            if(flag3 != 0)
+            {
+                printf("\n\n\t\t\tOperador A = %0.2f",A);
+                printf("\t\tOperador B = %0.2f\n\n",B);
                 printf("La suma de (A+B) es: %0.2f \n",suma);
                 printf("La resta de (A-B) es: %0.2f \n",resta) ;
                 printf("La multiplicacion de (A*B) es: %0.2f \n",multi) ;
-                if(num2 == 0)
+                if(B == 0)
                 {
-                    printf("No se puede dividir por 0");
+                    printf("No se puede dividir por 0\n\n");
                 }
                 else
                 {
                     printf("La division de (A/B) es: %0.2f \n",div) ;
                 }
-                //    printf("El factorial de A es: %0.2f \n",factorial);
-                //   printf("El factorial de B es: %0.2f \n",factorial2) ;
+                if(A < 0)
+                {
+                    printf("No se puede calcular el factorial de un numero negativo.\n") ;
+                }else
+                {
+                    printf("El factorial de A es: %d \n",factorialA) ;
+                }
+                if(B < 0)
+                {
+
+                printf("No se puede calcular el factorial de un numero negativo.\n");
+
+                }else
+                {
+                    printf("El factorial de B es: %d \n",factorialB) ;
+                }
                 system("pause");
                 system("cls") ;
-                break;
+            }else
+            {
+                printf("\n*Error* No se calcularon los resultados.\n\n");
+                system("pause");
+                system("cls");
             }
-
-        case 5:
-            printf("\n Saliendo \n") ;
-            system("pause") ;
-            opcion = 5 ;
             break;
-
-        default:
-
-            printf("Opcion invalida, ingrese un numero del uno al cinco. \n") ;
-            system("pause");
-            system("cls");
-
-
+        case 5: // opcion de salir
+            printf("\n\n\n**SALIENDO** ") ;
+            system("pause") ;
+            salir = 1;
+            break;
         }
-
-
     }
-    while(opcion !=5) ;
+    while(salir == 0) ;
+
+    return 0;
 }
